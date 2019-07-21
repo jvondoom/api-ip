a03d19b9d69d3f51453cdd5205cab94e01bb6e70
phazhion <phazhion@gmail.com>
2019-07-21 11:38:59 -0600

    Created the GET methods of the endpoint to return the IP of requester, with an optional greeting message

--- server.js
+++ server.js
@@ -2,6 +2,32 @@ const express = require('express');
 const properties = require('./config/properties');
 const app = express();
 
+app.get('/', (req, res) => {
+  const ipAddr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('x-hello-world', 'JvDM');
+  res.end(
+    JSON.stringify({
+      ip: ipAddr,
+    })
+  );
+});
+
+app.get('/:name', (req, res) => {
+  const name = req.params.name;
+  const ipAddr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('x-hello-world', 'JvDM');
+  res.end(
+    JSON.stringify({
+      ip: ipAddr,
+      greeting: `Good to see you again, ${name}`,
+    })
+  );
+});
+
 app.listen(properties.PORT, (req, res) => {
   console.log(`Server is running on ${properties.PORT} port.`);
 });
