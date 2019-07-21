8bc44e337b0cdc8849f231d63979fac2cefbd388
phazhion <phazhion@gmail.com>
2019-07-21 12:05:25 -0600

    Update the server.js to show the readme file in root

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+
+const fs = require('fs');
+const marked = require('marked');
+
 const properties = require('./config/properties');
+
 const app = express();
 
 app.get('/', (req, res) => {
+  const path = __dirname + '/README.md';
+  const file = fs.readFileSync(path, 'utf8');
+  res.send(marked(file.toString()));
+})
+
+app.get('/ip', (req, res) => {
   const ipAddr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
   res.setHeader('Content-Type', 'application/json');
@@ -14,7 +25,7 @@ app.get('/', (req, res) => {
   );
 });
 
-app.get('/:name', (req, res) => {
+app.get('/ip:name', (req, res) => {
   const name = req.params.name;
   const ipAddr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
@@ -28,6 +39,6 @@ app.get('/:name', (req, res) => {
   );
 });
 
-app.listen(4000, (req, res) => {
-  console.log(`Server is running on 4000 port.`);
+app.listen(properties.PORT, (req, res) => {
+  console.log(`Server is running on ${properties.PORT} port.`);
 });
